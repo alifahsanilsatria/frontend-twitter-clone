7a2a168afa8352b227f37a6e82e9ae1f7a3c03cc
Alif Ahsanil Satria <alif.ahsanil@bluebirdgroup.com>
2024-08-18 16:20:41 +0700

    add progress in login form

--- twitter-clone/app/i/flow/login/page.tsx
+++ twitter-clone/app/i/flow/login/page.tsx
@@ -8,12 +8,12 @@ export default function LoginForm() {
         <img src="/images/x_logo.png" alt="X Logo" className="login-form-header__x-logo border-white border-2"/>
       </div>
       <div className="login-form-content border-white border-2">
-        <p className="login-form-content__sign-in-to-x-wording">Sign in to X</p>
-        <form className="login-form-content__user-input-and-submit">
+        <p className="login-form-content__sign-in-to-x-wording border-white border-2">Sign in to X</p>
+        <form className="login-form-content__user-input-and-submit border-white border-2">
           <input className="login-form-content__user-input-and-submit__username" type="text" placeholder="Phone, email, or username"></input>
-          <button type="submit" className="bg-white login-form-content__user-input-and-submit__next-button">Next</button>
+          <button type="submit" className="login-form-content__user-input-and-submit__next-button">Next</button>
         </form>
-        <p className="login-form-content__sign-up-suggestion text-gray-500">Don't have an account? <a>Sign up</a></p>
+        <p className="login-form-content__sign-up-suggestion border-white border-2 text-gray-500">Don't have an account? <a>Sign up</a></p>
       </div>
     </div>
   );
